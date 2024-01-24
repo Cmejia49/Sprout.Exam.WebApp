b16379684eb9409dc5c3097477b3ac3264b48b80
chris <chris@avironsoftware.com>
2024-01-25 02:20:45 +0800

    Add project files.

---
+++ Sprout.Exam.Domain/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sprout.Exam.Domain
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FullName { get; set; }
+    }
+}
